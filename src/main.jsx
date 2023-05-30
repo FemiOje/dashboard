0ec2f2f7a010681f8802c57098b5c65f7d40af6f
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2023-05-30 11:11:40 +0100

    made Home.jsx default outlet

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />}>
-            <Route path="home" element={<Home />} />
+            <Route default path="/" element={<Home />} />
             <Route path="insights" element={<Insights />} />
             <Route path="messages" element={<Messages />} />
             <Route path="favorites" element={<Favorites />} />
