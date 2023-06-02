a24ed7e12a3b3df082cf5e35cbf1afdc94a0c9ea
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2023-06-02 23:42:10 +0100

    add: PageNotFound.jsx

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import Navbar from './Navbar';
+import PageNotFoundGif from '../assets/images/page-not-found.gif';
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <div className='bg-[#DAF0FF] h-screen text-center'>
+      <Navbar />
+
+      <img src={PageNotFoundGif} alt="page-not-found" className='m-auto w-[100vw] h-[100vh]' />
+
+      <div className="absolute bottom-[2vh] w-full ml-auto">
+        <p className='text-xl text-semibold m-5'>This page does not exist</p>
+        <Link to={'/'} className='text-blue-700 font-semibold m-5'>Back To Homepage</Link>
+      </div>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -9,22 +9,23 @@ import Favorites from './routes/Favorites';
 import Explore from './routes/Explore';
 import Marketplace from './routes/Marketplace';
 import ComingSoon from './components/ComingSoon';
+import PageNotFound from './components/PageNotFound';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route default path="/" element={<Home />} />
-            <Route path="insights" element={<Insights />} />
-            <Route path="favorites" element={<Favorites />} />
-            <Route path="explore" element={<Explore />} />
-            <Route path="marketplace" element={<Marketplace />} />
-          </Route>
-          
-          <Route path='/coming-soon' element={<ComingSoon />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route default path="/" element={<Home />} />
+          <Route path="insights" element={<Insights />} />
+          <Route path="favorites" element={<Favorites />} />
+          <Route path="explore" element={<Explore />} />
+          <Route path="marketplace" element={<Marketplace />} />
+        </Route>
+        <Route path='/coming-soon' element={<ComingSoon />} />
+        <Route path='/*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
