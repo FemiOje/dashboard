c93ce7e0021e06d7dd4ec59325c1bf2c37202667
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2023-06-02 22:48:22 +0100

    style: coming soon page

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ComingSoonGif from '../assets/images/coming-soon.gif';
-// import Navbar from './Navbar';
 
 const ComingSoon = () => {
     return (
         <div className='h-screen bg-white'>
-            {/* <Navbar /> */}
             <div className='text-center bg-white pt-10'>
                 <img src={ComingSoonGif} className="m-auto md:w-[30vw] md:h-[40vh]" alt="coming-soon" />
                 <p className='text-2xl mb-5 p-3'>Coming Soon...</p>
