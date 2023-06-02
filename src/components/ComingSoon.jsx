5e939ba5725849d6bd4759b70559975927c68584
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2023-06-02 23:48:24 +0100

    text change

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -7,7 +7,7 @@ const ComingSoon = () => {
         <div className='h-screen bg-white'>
             <div className='text-center bg-white pt-10'>
                 <img src={ComingSoonGif} className="m-auto md:w-[30vw] md:h-[40vh]" alt="coming-soon" />
-                <p className='text-2xl mb-5 p-3'>Coming Soon...</p>
+                <p className='text-2xl mb-5 p-3'>Coming Soon... or never </p>
                 <p>Please check back later</p>
                 <Link to={'/'} className='text-blue-700 font-semibold'>Back To Homepage</Link>
             </div>
